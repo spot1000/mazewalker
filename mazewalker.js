c6da6c74b1369adf760bb356227a65f41a9b6fbf
Adam Lichter <adamlichter@gmail.com>
2017-02-02 21:42:02 -0330

    collision detection working

--- mazewalker.js
+++ mazewalker.js
@@ -1,3 +1,19 @@
+const TILE_WIDTH = 32
+
+var walls = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+						 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+					   [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+						 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+					   [1,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1],
+					   [1,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1],
+						 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+						 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+						 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+						 [1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+						 [1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+						 [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+					   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
+
 var canvas = document.getElementById('maze');
 var ctx = canvas.getContext('2d');
 
@@ -30,25 +46,11 @@ function startGame() {
 			y: 175,
 			vx: 0,
 			vy: 0
-		},
-		walls: {
-			wall1: {
-				topX: 100,
-				topY: 100,
-				xLength: 50,
-				yLength: 200
-			},
-			wall2: {
-				topX: 400,
-				topY: 100,
-				xLength: 50,
-				yLength: 200
-			}
 		}
 	}
-	console.log(state.walls.wall1)
-
-	window.setInterval(gameLoop, 10);
+	// console.log(state.walls.wall1)
+	//gameLoop()
+	 window.setInterval(gameLoop, 10);
 }
 
 function gameLoop() {
@@ -60,21 +62,16 @@ function gameLoop() {
 function drawScreen() {
 	ctx.clearRect(0, 0, 640, 400);
 	ctx.drawImage(man, state.player.x, state.player.y, 30, 30);
-	drawWall(state.walls.wall1.topX, state.walls.wall1.topY, state.walls.wall1.xLength, state.walls.wall1.yLength)
-	// for (var wall in state.walls) {
-	// 	console.log(state.walls.hasOwnProperty(wall))
-	// 	if (state.walls.hasOwnProperty(wall)) {
-	// 		for (var obj in wall) {
-	// 			console.log(obj)
-	// 		}
-	// 	}
-	// }
+	drawWalls(walls)
 }
 
 function updateState() {
-	state.player.x += state.player.vx;
-	state.player.y += state.player.vy;
-	collisionDetection();
+	let newX = state.player.x + state.player.vx;
+	let newY = state.player.y + state.player.vy;
+	if (!collisionDetection(walls, newX, newY)) {
+		state.player.x = newX;
+		state.player.y = newY;
+	}
 }
 
 function drawWall(topX, topY, xLength, yLength) {
@@ -84,26 +81,41 @@ function drawWall(topX, topY, xLength, yLength) {
     ctx.fill();
     ctx.closePath();
 }
-function collisionDetection() {
-	if (state.player.y < 10)
-		state.player.y = 10;
-	if (state.player.y > 360)
-		state.player.y = 360;
-	if (state.player.x < 10)
-		state.player.x = 10;
-	if (state.player.x > 600)
-		state.player.x  = 600;
 
-	//wall detection
-	if ((state.player.x <= state.walls.wall1.topX + state.walls.wall1.xLength && state.player.x >= state.walls.wall1.topX) && (state.player.y <= state.walls.wall1.topY + state.walls.wall1.yLength && state.player.y >= state.walls.wall1.topY)) {
-
-		if(state.player.x > state.walls.wall1.topX + state.walls.wall1.xLength) {
-			state.player.x = state.walls.wall1.topX + state.walls.wall1.xLength
+function drawWalls(walls) {
+	for (var column = 0; column < walls.length; column++) {
+		for (var row = 0; row < walls[column].length; row++) {
+			if (walls[column][row]) {
+				drawWall(row * TILE_WIDTH, column * TILE_WIDTH, TILE_WIDTH, TILE_WIDTH)
+			}
 		}
-
-		state.player.x = state.walls.wall1.topX + state.walls.wall1.xLength
 	}
 }
+
+function collisionDetection(walls, newX, newY) {
+	if (newY < 10)
+		return true;
+	if (newY > 360)
+		return true;
+	if (newX < 10)
+		return true;
+	if (newX > 600)
+		return true;
+
+	for (var column = 0; column < walls.length; column++) {
+		for (var row = 0; row < walls[column].length; row++) {
+			if (newX > row * TILE_WIDTH - 32 && newX < row * TILE_WIDTH + TILE_WIDTH && newY > column * TILE_WIDTH - 32 && newY < column * TILE_WIDTH + TILE_WIDTH && walls[column][row] == 1 ) {
+				return true
+			}
+		}
+	}
+
+	//wall detection
+	// if (newX < state.walls.wall1.topX + state.walls.wall1.xLength && newX > state.walls.wall1.topX - 30 && newY < state.walls.wall1.topY + state.walls.wall1.yLength && newY > state.walls.wall1.topY - 30) {
+	// 	return true;
+	// }
+}
+
 
 window.onkeyup = (event) => {
 	if (state.state == "playing") {
